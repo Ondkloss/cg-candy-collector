9a1574b3dcdce6ee5e90a650701627ad6186f3ab
Halvor Holsten Strand <halvor.holsten.strand@gmail.com>
2024-05-24 16:32:47 +0200

    Additional changes towards #2. Should now properly detect day change.

--- src/content.js
+++ src/content.js
@@ -1,10 +1,18 @@
+let LAST_TIME = Number.MAX_VALUE
+
 const message = text => {
   chrome.runtime.sendMessage(text)
 }
 
 const reload = () => {
-  message('reloading')
   location.reload()
+}
+
+const calculateSecondsRemaining = text => {
+  const timePart = text.split(' ')[0]
+  const hhmmss = timePart.split(':');
+  const seconds = ((+hhmmss[0]) * 60 * 60) + ((+hhmmss[1]) * 60) + (+hhmmss[2]);
+  return seconds
 }
 
 const checkCandy = () => {
@@ -13,17 +21,31 @@ const checkCandy = () => {
   if (buttonElement && !buttonElement.disabled) {
     message('clicking')
     buttonElement.click()
+    message('reloading')
     setTimeout(reload, 5000)
   }
   else if (buttonElement && buttonElement.disabled) {
     message('scouting')
     const cooldown = buttonElement.firstElementChild
-    if (cooldown?.innerHTML && cooldown.innerHTML.startsWith('0:00:00')) {
-      reload()
+
+    if (cooldown?.innerHTML) {
+      const seconds = calculateSecondsRemaining(cooldown.innerHTML)
+
+      if (LAST_TIME < seconds) {
+        message('reloading')
+        reload()
+      }
+      else {
+        message(`${seconds} seconds remaining`)
+        LAST_TIME = seconds
+      }
+    }
+    else {
+      message('missing cooldown element')
     }
   }
   else {
-    message('could not find expected elements')
+    message('missing button element')
   }
 }
 
